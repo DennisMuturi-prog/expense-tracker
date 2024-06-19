feb8276ccd19f949eb441b64902692e4d2fab4d5
Dennis Muturi <denismutur@gmail.com>
2024-06-19 18:22:42 +0300

    chore: Update date format in expense data
    ```

--- src/app/components/total-cost/total-cost.component.ts
+++ src/app/components/total-cost/total-cost.component.ts
@@ -1,24 +1,34 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject ,AfterViewInit, ViewChild} from '@angular/core';
 import { ExpensesService } from '../../services/expenses-service.service';
 import { Expense } from '../../Types/ExpenseType';
-import {MatTableModule} from '@angular/material/table'
+import {MatTableModule,MatTableDataSource} from '@angular/material/table'
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-total-cost',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [MatTableModule,MatSortModule,DatePipe],
   templateUrl: './total-cost.component.html',
   styleUrl: './total-cost.component.css'
 })
-export class TotalCostComponent {
+export class TotalCostComponent  implements AfterViewInit{
   expensesService=inject(ExpensesService);
   expenses:Expense[]=[]
   totalcost:number=0
-  columnsToDisplay:string[]=['id','expenseDescription','dateIncurred','amount','necessary']
+  columnsToDisplay:string[]=['id','expenseDescription','dateIncurred','necessary','amount']
+  dataSource=new MatTableDataSource(this.expenses)
+  @ViewChild(MatSort) sort!:MatSort;
+  ngAfterViewInit(): void {
+    this.dataSource.sort=this.sort
+  }
   ngOnInit():void{
     this.expensesService.getExpenses().subscribe((res)=>{
-      console.log(typeof(res[3].amount))
       this.expenses=res;
+      this.expenses.map((expense)=>{
+        return {...expense,dateIncurred:new Date(expense.dateIncurred)}
+      })
+      this.dataSource.data=this.expenses
       this.totalcost=res.reduce((total,currentValue)=>(total+currentValue.amount),0)
     })
 
