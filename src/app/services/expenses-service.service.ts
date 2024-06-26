00ed82980dab5308782fdd2360cf65aada3f69f8
Dennis Muturi <denismutur@gmail.com>
2024-06-26 16:12:05 +0300

    chore: Update expenses-service to import signal from @angular/core and Observable from rxjs

--- src/app/services/expenses-service.service.ts
+++ src/app/services/expenses-service.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Injectable, inject,signal } from '@angular/core';
+import { Observable} from 'rxjs';
 import { Expense } from '../Types/ExpenseType';
 
 @Injectable({
