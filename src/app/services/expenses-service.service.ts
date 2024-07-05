8d068b072a8e30f5d8afa9a86cc12ec4988f864e
Dennis Muturi <denismutur@gmail.com>
2024-07-05 11:06:01 +0300

    Update expenses-service to use observables for CRUD operations

--- src/app/services/expenses-service.service.ts
+++ src/app/services/expenses-service.service.ts
@@ -1,24 +1,80 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject,signal } from '@angular/core';
-import { Observable} from 'rxjs';
-import { Expense } from '../Types/ExpenseType';
+import { BehaviorSubject, EMPTY, Observable, Subject, combineLatest, map, merge, of, scan, switchMap} from 'rxjs';
+import { Expense, ModifyAction } from '../Types/ExpenseType';
+import { tap } from 'rxjs';
+import { Action } from 'rxjs/internal/scheduler/Action';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ExpensesService {
-  http=inject(HttpClient)
-  constructor() { }
-  getExpenses():Observable<Expense[]>{
-    return this.http.get<Expense[]>('http://localhost:3000/expenses')
+  http = inject(HttpClient);
+  deleteExpenseAction$ = new Subject<number | string>();
+  updateExpenseAction$ = new Subject<Expense>();
+  addExpenseAction$ = new Subject<Expense>();
+  deleteExpense$ = this.deleteExpenseAction$.pipe(
+    switchMap((id) => this.deleteExpense(id))
+  );
+  updateExpense$ = this.updateExpenseAction$.pipe(
+    switchMap((expense) => this.updateExpense(expense))
+  );
+  addExpense$ = this.addExpenseAction$.pipe(
+    switchMap((expense) => this.addExpense(expense))
+  );
+  deleteHandler(expense: Expense) {
+    return (state: Expense[]) => [
+      ...state.filter((expenseitem) => expenseitem.id !== expense.id),
+    ];
   }
-  addExpense(newExpense:Expense):Observable<Expense>{
-    return this.http.post<Expense>('http://localhost:3000/expenses',newExpense)
+  updateHandler(expense: Expense) {
+    return (state: Expense[]) => [
+      ...state.map((expenseitem) => {
+        if (expenseitem.id == expense.id) {
+          return expense;
+        } else {
+          return expenseitem;
+        }
+      }),
+    ];
   }
-  deleteExpense(expenseId:string | number):Observable<Expense>{
-    return this.http.delete<Expense>(`http://localhost:3000/expenses/${expenseId}`)
+  addHandler(expense: Expense) {
+    return (state: Expense[]) => [...state, expense];
   }
-  updateExpense(newExpense:Expense):Observable<Expense>{
-    return this.http.put<Expense>(`http://localhost:3000/expenses/${newExpense.id}`,newExpense)
+  firstExpensesHandler(expenses: Expense[]) {
+    return (state: Expense[]) => [...state, ...expenses];
   }
+  firstexpenses$ = this.getExpenses();
+  expenses$=merge(
+    this.firstexpenses$.pipe(map(this.firstExpensesHandler)),
+    this.deleteExpense$.pipe(map(this.deleteHandler)),
+    this.addExpense$.pipe(map(this.addHandler)),
+    this.updateExpense$.pipe(map(this.updateHandler))
+  ).pipe(
+    scan(
+      (state:Expense[],stateHandler)=>stateHandler(state),
+      []
+    )
+  )
+  getExpenses(): Observable<Expense[]> {
+    return this.http.get<Expense[]>('http://localhost:3000/expenses');
+  }
+  addExpense(newExpense: Expense): Observable<Expense> {
+    return this.http.post<Expense>(
+      'http://localhost:3000/expenses',
+      newExpense
+    );
+  }
+  deleteExpense(expenseId: number | string): Observable<Expense> {
+    return this.http.delete<Expense>(
+      `http://localhost:3000/expenses/${expenseId}`
+    );
+  }
+  updateExpense(newExpense: Expense): Observable<Expense> {
+    return this.http.put<Expense>(
+      `http://localhost:3000/expenses/${newExpense.id}`,
+      newExpense
+    );
+  }
+  constructor() {}
 }
