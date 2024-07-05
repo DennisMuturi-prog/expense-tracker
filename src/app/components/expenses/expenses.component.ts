3fc8370381c688a7a2ed0ba5fc80b8f07d1f1f5f
Dennis Muturi <denismutur@gmail.com>
2024-07-05 11:05:49 +0300

    chore: Update expenses component to use expenses$ observable

--- src/app/components/expenses/expenses.component.ts
+++ src/app/components/expenses/expenses.component.ts
@@ -4,39 +4,31 @@ import { Expense } from '../../Types/ExpenseType';
 import { ExpenseItemComponent } from '../expense-item/expense-item.component';
 import { AddExpenseComponent } from '../add-expense/add-expense.component';
 import { HeaderComponent } from '../header/header.component';
+import { AsyncPipe } from '@angular/common';
 
 
 @Component({
   selector: 'app-expenses',
   standalone: true,
-  imports: [ExpenseItemComponent,AddExpenseComponent,HeaderComponent],
+  imports: [ExpenseItemComponent,AddExpenseComponent,HeaderComponent,AsyncPipe],
   templateUrl: './expenses.component.html',
   styleUrl: './expenses.component.css'
 })
 export class ExpensesComponent {
   expensesService=inject(ExpensesService)
   showAddExpense:boolean=false
-  expenses!:Expense[]
-  ngOnInit():void{
-    this.expensesService.getExpenses().subscribe((res)=>{
-      this.expenses=res
-    })
-  }
+  expenses$=this.expensesService.expenses$
+ 
   addExpense(newExpense:Expense){
-    this.expensesService.addExpense(newExpense).subscribe((res)=>{
-      this.expenses.push(res)
-    })
+    this.expensesService.addExpenseAction$.next(newExpense)
   }
   deleteExpense(expenseId:string | number){
-    this.expensesService.deleteExpense(expenseId).subscribe((res)=>{
-      console.log(res)
-      this.expenses=this.expenses.filter(expense=>expense.id!==res.id)
-    })
+    this.expensesService.deleteExpenseAction$.next(expenseId)
   }
   toggleAddExpense(newShow:boolean){
     this.showAddExpense=newShow
   }
   updateExpense(updatedExpense:Expense){
-    this.expensesService.updateExpense(updatedExpense).subscribe()
+    this.expensesService.updateExpenseAction$.next(updatedExpense)
   }
 }
