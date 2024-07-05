baaf5f3b55d0f55d29dd73358f24ee70ea5cda2e
Dennis Muturi <denismutur@gmail.com>
2024-07-05 13:39:55 +0300

    Refactor necessity-index component to use expenses$ observable

--- src/app/components/necessity-index/necessity-index.component.ts
+++ src/app/components/necessity-index/necessity-index.component.ts
@@ -2,46 +2,30 @@ import { Component, computed, inject } from '@angular/core';
 import { ExpensesService } from '../../services/expenses-service.service';
 import { Expense } from '../../Types/ExpenseType';
 import { ExpenseItemComponent } from '../expense-item/expense-item.component';
-import { signal } from '@angular/core';
+import {map } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-necessity-index',
   standalone: true,
-  imports: [ExpenseItemComponent],
+  imports: [ExpenseItemComponent,AsyncPipe],
   templateUrl: './necessity-index.component.html',
   styleUrl: './necessity-index.component.css',
 })
 export class NecessityIndexComponent {
   expensesService = inject(ExpensesService);
-  allExpenses = signal<Expense[]>([]);
-  necessaryExpenses = computed(() =>
-    this.allExpenses().filter((expense) => expense.necessary == true)
+  expenses$ = this.expensesService.expenses$;
+  necessaryExpenses$ = this.expenses$.pipe(
+    map((expenses) => expenses.filter((expense) => expense.necessary == true))
   );
-  unnecessaryExpenses = computed(() =>
-    this.allExpenses().filter((expense) => expense.necessary == false)
+  unnecessaryExpenses$ = this.expenses$.pipe(
+    map((expenses) => expenses.filter((expense) => expense.necessary == false))
   );
-  ngOnInit(): void {
-    this.expensesService.getExpenses().subscribe((res) => {
-      this.allExpenses.set(res);
-    });
-  }
+
   deleteExpense(expenseId: string | number) {
-    this.expensesService.deleteExpense(expenseId).subscribe((res) => {
-      this.allExpenses.update(previous=>previous.filter(expense=>expense.id!==expenseId))
-    });
+    this.expensesService.deleteExpenseAction$.next(expenseId);
   }
   updateExpense(updatedExpense: Expense) {
-    this.expensesService.updateExpense(updatedExpense).subscribe((res) => {
-      this.allExpenses.update(previous=>previous.map(expense=>{
-        if(expense.id==res.id){
-          return res
-        }
-        else{
-          return expense
-        }
-      }))
-
-    }
-      );
+    this.expensesService.updateExpenseAction$.next(updatedExpense);
   }
 }
